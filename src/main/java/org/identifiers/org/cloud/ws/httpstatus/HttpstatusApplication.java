cc5caa6a09f7d2a5c1925eaf65085339030861af
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-03 05:15:20 +0000

    project structure

---
+++ src/main/java/org/identifiers/org/cloud/ws/httpstatus/HttpstatusApplication.java
@@ -0,0 +1,12 @@
+package org.identifiers.org.cloud.ws.httpstatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpstatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpstatusApplication.class, args);
+	}
+}
