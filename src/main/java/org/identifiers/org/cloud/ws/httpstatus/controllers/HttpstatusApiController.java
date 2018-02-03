34012c918ec8534a26c8e62ee4f3fcca32457415
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-03 05:16:19 +0000

    main API controller

---
+++ src/main/java/org/identifiers/org/cloud/ws/httpstatus/controllers/HttpstatusApiController.java
@@ -0,0 +1,11 @@
+package org.identifiers.org.cloud.ws.httpstatus.controllers;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: httpstatus
+ * Package: org.identifiers.org.cloud.ws.httpstatus.controllers
+ * Timestamp: 2018-02-03 5:15
+ * ---
+ */
+public class HttpstatusApiController {
+}
